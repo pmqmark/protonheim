7ba44bb497d3957b5c3ece2d378f0cfea2e89095
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-08-02 16:47:55 +0530

    about us

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ServicePage from './page/ServicePage';
 import Layout from './util/Layout';
 import ProductPage from './page/ProductPage';
+import AboutPage from './page/AboutPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/services' element={<ServicePage />} />
             <Route path='/products' element={<ProductPage />} />
+            <Route path='/about' element={<AboutPage />} />
             </Route>
         </Routes>
       </Router>
