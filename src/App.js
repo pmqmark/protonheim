137292fa6b2737d2a8336add920ebed547706c03
Meharunnisasalim <142387481+Meharunnisasalim@users.noreply.github.com>
2024-08-03 17:14:17 +0530

    footer ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Home from './page/Home';
+import './css/global.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ServicePage from './page/ServicePage';
 import Layout from './util/Layout';
 import ProductPage from './page/ProductPage';
 import AboutPage from './page/AboutPage';
 import ContactusPage from './page/ContactusPage';
-
+import usePageColors from './hooks/usePageColors';
 
 function App() {
+  
   return (
     <div className="App">
       <Router>
+        <PageColorsWrapper/>
         <Routes>
             <Route path='' element={<Layout/>}>
             <Route path='/' element={<Home />} />
@@ -25,5 +28,8 @@ function App() {
     </div>
   );
 }
-
+function PageColorsWrapper() {
+  usePageColors();
+  return null; // This component does not render anything, just sets the colors
+}
 export default App;
